1f049cf36d26984883fa9aa08a7799ba85990200
nayow <nayow@hotmail.fr>
2020-02-17 20:34:47 +0100

    first commit

---
+++ app/models/event.rb
@@ -0,0 +1,12 @@
+class Event < ApplicationRecord
+  has_many :attendances
+  has_many :users, through: :attendances
+
+  validates :start_date, presence: true
+  validates :duration, presence: true, if -> {duration>0 && duration%5==0}
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, if -> {[1..1000].include?.price}
+  validates :location, presence: true
+
+end
