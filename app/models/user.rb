e3df9afbcea726f49a53398387816073195dbadc
nayow <nayow@hotmail.fr>
2020-02-17 21:25:05 +0100

    mail works

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
 
   after_create :welcome_send
 
+  validates :first_name, presence: true
+  validates :email, presence: true
+
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
