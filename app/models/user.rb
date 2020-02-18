6bf4c6395303fe13fcd18acd6c78f8944d3952a5
nayow <nayow@hotmail.fr>
2020-02-18 22:35:34 +0100

    removed mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   has_many :admins, class_name: 'Event', foreign_key: 'event_id'
 
   validates :email, presence: true
-  after_create :welcome_send
+  # after_create :welcome_send
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  
+
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
