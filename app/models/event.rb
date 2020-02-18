653a354b89210f258b0b07d142a7ca6e3f419dfe
nayow <nayow@hotmail.fr>
2020-02-18 11:04:36 +0100

    heroku

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,23 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
 
-  # validates :start_date, presence: true
-  # validates :duration, presence: true, if -> {duration>0 && duration%5==0}
-  # validates :title, presence: true, length: { in: 5..140 }
-  # validates :description, presence: true, length: { in: 20..1000 }
-  # validates :price, presence: true, if -> {[1..1000].include?.price}
-  # validates :location, presence: true
+  validates :start_date, presence: true
+  validates :duration, presence: true, if: :start_date_cannot_be_in_the_past && :duration_must_be_multiple_of_5 
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, if -> {[1..1000].include?(price)}
+  validates :location, presence: true
+
+  def start_date_cannot_be_in_the_past
+		if !start_date.blank? && start_date < Date.today
+      	errors.add(:start_date, "can't be in the past")
+    	end
+  end
+  
+	def duration_must_be_multiple_of_5
+		if duration % 5 != 0
+		errors.add(:duration, "must be a mutiple of 5")
+		end
+	end
 
 end
